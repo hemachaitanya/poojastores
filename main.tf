48bf6b820baf79c4a10509a231a653b340bf6b49
hemachaitanya <b.hemalatha203@gmail.com>
2024-02-20 09:50:50 +0530

    add changes

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "aws_vpc" "vpc1" {
 resource "aws_subnet" "sub" {  
     vpc_id = aws_vpc.vpc1.name
     count = 3
-    name = "mysub[count.index]"
-    cidr_block =  190.19.[count.index].0/24
+    name = mysub$(count.index)
+    cidr_block =  cidrsubnet("190.19.0.0/16",8,count.index)
     tags = {
-      Name = "mysub[count.index]"
+      Name = "mysub$(count.index)"
     }
 
 }
